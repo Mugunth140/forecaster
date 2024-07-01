2cb3cbecb8a0b997bc737569a9335c16c9885b89
Mugunth140 <soulcyber148@gmail.com>
2024-07-01 13:36:31 +0530

    refactored navbar component

--- app/components/Navbar/Navbar.jsx
+++ app/components/Navbar/Navbar.jsx
@@ -2,23 +2,47 @@ import styles from "./Navbar.module.scss";
 import { MdLocationPin } from "react-icons/md";
 import { FiSearch } from "react-icons/fi";
 import Darkmode from "../Darkmode/Darkmode";
+import { useState } from 'react';
 
-const Navbar = () => {
+const Navbar = ({ onCity, onCitySubmit }) => {
+  const [inputValue, setInputValue] = useState('');
+
+  const handleChange = (event) => {
+    const value = event.target.value;
+    setInputValue(value);
+    onCity(value);
+  };
+
+  const handleSubmit = () => {
+    onCitySubmit();
+  };
+
   return (
     <nav className={styles.navContainer}>
-      <div className={styles.navLocationContainer}>
-        <MdLocationPin className={styles.navLocation} />
-        Coimbatore
-      </div>
-      <div className={styles.navSearch}>
-        <input className="" type="text" placeholder="search on forecast.." />
-        <button>
-          <FiSearch />
-        </button>
-      </div>
-      <div className={styles.darkmodeContainer}>
-        <Darkmode />
-      </div>
+      <section>
+        <div className={styles.navLocationContainer}>
+          <MdLocationPin className={styles.navLocation} />
+          Coimbatore
+        </div>
+        <div className={styles.darkmodeContainer}>
+          <Darkmode />
+        </div>
+      </section>
+       
+      <aside>
+        <div className={styles.navSearch}>
+          <input
+            className=""
+            type="text"
+            value={inputValue}
+            onChange={handleChange}
+            placeholder="search on forecast..."
+          />
+          <button onClick={handleSubmit}>
+            <FiSearch />
+          </button>
+        </div>
+      </aside>
     </nav>
   );
 };
--- app/page.jsx
+++ app/page.jsx
@@ -1,37 +1,41 @@
 'use client'
 import Navbar from '@/app/components/Navbar/Navbar';
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 function Home() {
   const [weather, setWeather] = useState(null);
-  const apiKey = process.env.NEXT_PUBLIC_API_KEY
-  const city = "Coimbatore";
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+  const [city, setCity] = useState("");
+  const apiKey = process.env.NEXT_PUBLIC_API_KEY;
 
-  useEffect(() => {
-    fetch(url)
-      .then((res) => res.json())
-      .then((data) => {
-        setWeather(data); 
-      })
-      .catch((err) => console.log(err));
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []); 
+  const handleCitySubmit = () => {
+    if (city) { // Only fetch weather if city is provided
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+
+      fetch(url)
+        .then((res) => res.json())
+        .then((data) => {
+          setWeather(data);
+        })
+        .catch((err) => console.log(err));
+    }
+  };
+
+  const handleCityChange = (cityData) => {
+    setCity(cityData);
+  };
 
   return (
     <>
-      {weather && (
-        <>
-        <Navbar />
-       <div className="bg-gradient-to-r from-[#62cff4] to-[#2c67f2]">
-        <div >
-          <h2 >Weather in {city}</h2>
-          <p >Temperature: {weather.main.temp} °C</p>
-          <p >Description: {weather.weather[0].description}</p>
-        </div>
-    </div>
-    </>
-      )}
+      <Navbar onCity={handleCityChange} onCitySubmit={handleCitySubmit} />
+      <div className="bg-gradient-to-r from-[#62cff4] to-[#2c67f2]">
+        {weather && weather.main && weather.weather && weather.weather[0] && (
+          <div>
+            <h2>Weather in {city}</h2>
+            <p>Temperature: {weather.main.temp} °C</p>
+            <p>Description: {weather.weather[0].description}</p>
+          </div>
+        )}
+      </div>
     </>
   );
 }
